db8f6f141aed67ba92daff8dbedd4c2407625623
Seshanand <seshanand04@gmail.com>
2026-07-06 18:19:55 +0530

    Initial Commit

---
+++ Controllers/EnvironmentController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EnvironmentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _environment;
+
+        public EnvironmentController(
+            IConfiguration configuration,
+            IWebHostEnvironment environment)
+        {
+            _configuration = configuration;
+            _environment = environment;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                CurrentEnvironment = _environment.EnvironmentName,
+                EnvironmentMessage = _configuration["EnvironmentName"]
+            });
+        }
+    }
+}
