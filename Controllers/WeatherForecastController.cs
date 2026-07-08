f84c56bb2f243433fb2a5fd49843c1d7625668c1
Seshanand <seshanand04@gmail.com>
2026-07-08 14:19:53 +0530

    change in weather file

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -25,7 +25,7 @@ namespace ProductService.Controllers
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = "Deployed from GitHub Actio"
+                Summary = "Deployed from GitHub Actionn"
             })
             .ToArray();
         }
